bd637fcff26a40d66ab5e0c3a0d061b92373e8e2
mohit <mohitbhusal11@gmail.com>
2023-10-17 23:14:41 +0545

    16 json

--- 16JSON/index.js
+++ 16JSON/index.js
@@ -12,13 +12,25 @@ const recipeJSON =
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let data;
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", {recipe: data});
 });
 
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
   //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+  var temp = req.body.choice;
+  if(temp === "chicken"){
+    data = JSON.parse(recipeJSON)[0];
+  }
+  else if(temp == "beef"){
+    data = JSON.parse(recipeJSON)[1];
+  }
+  else if(temp == "fish"){
+    data = JSON.parse(recipeJSON)[2];
+  }
 });
 
 app.listen(port, () => {
