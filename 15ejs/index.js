fc04456d3ee059c455fb0bc8386ecf6cc93a2a86
mohit <mohitbhusal11@gmail.com>
2023-10-18 13:10:17 +0545

    18 ejs tags

--- 15ejs/index.js
+++ 15ejs/index.js
@@ -4,6 +4,26 @@ import ejs from "ejs"
 const app = express();
 const port = 3000;
 
+const date = new Date();
+const day = date.getDay();
+
+const days = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+
+app.get("/", (req, res) =>{
+    if(day == 0 || day == 6){
+        res.render('index.ejs', {
+            day : days[day],
+            message : "have fun"
+        })
+    }
+    else{
+        res.render('index.ejs', {
+            day : days[day],
+            message : "work hard"
+        })
+    }
+})
 
 app.listen(port, ()=>{
     console.log("server is running")
